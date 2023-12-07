8d9279650e66a9a55754c10555ffa917184d484c
Jayram Kumar <jayram14032001@gmail.com>
2023-12-07 16:32:09 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-// require('dotenv').config({path:'./env'})
 import dotenv from "dotenv"
-// import mongoose from "mongoose";
-// import { DB_NAME } from "./constants";
-
 import connectDB from "./db/index.js";
 // First approach to connect the database 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+//method 1  using try-catch
+
+// const asyncHandler = (requestHandler) => async (err,req,res,next) => {
+//     try {
+//         await requestHandler(req, res, next);
+        
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success:false,
+//             message:err.message
+//         })
+//     }
+// }
+
+
+
+//method 2 using promise
+const asyncHandler = (requestHandler) => {
+    (err,req,res, next)=>{
+        Promise.resolve(requestHandler(req, res, next)).
+        catch((err)=> next(err))
+    }
+}
+
+export {asyncHandler}
