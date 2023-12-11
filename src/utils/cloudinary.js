4eaac4570fcfb2c0f16dd0ec8fcb756580a68a87
Jayram Kumar <jayram14032001@gmail.com>
2023-12-12 02:02:21 +0530

    creating middleware with multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+ import { v2 } from "cloudinary";
+ import fs from "fs"
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET, 
+});
+
+
+const updloadOnCloudinary = async(localFilePath) => {
+    try {
+        if(!localFilePath) return null;
+        //otherwise upload file on cloudinary
+
+        const response = await cloudinary.v2.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        //file has been uploaded sucessfully
+        console.log("File is uploaded on cloudinary",response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath) //remove the locally saved temporarly file as the upload operation got failed
+        return null
+
+        
+    }
+}
+
+export {cloudinary}
