6651b68063de997f5ec72fb7f594c7313f51cf03
Jayram Kumar <jayram14032001@gmail.com>
2024-01-03 16:24:58 +0530

    Subscription Schema design

---
+++ src/models/subscription.model.js
@@ -0,0 +1,15 @@
+import mongoose, {Schema} from "mongoose";
+
+const subscriptionSchema = new Schema({
+    subscriber: {
+        type: Schema.Types.ObjectId, //one who is subscribing
+        ref: "User"
+    },
+
+    channel: {
+        type: Schema.Types.ObjectId, //one to whom 'subscriber' is subscribe
+        ref: "User"
+    }
+})
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
