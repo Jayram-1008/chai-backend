17a13f2bae3b10aba8704171bb74c0791e46fe82
Jayram Kumar <jayram14032001@gmail.com>
2024-01-03 17:04:33 +0530

    update account detils controller build

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -249,10 +249,45 @@ const changeCurrentPassword = asyncHandler(async (req, res) =>{
     .json(new ApiResponse(200, {}, "Password change successfully"))
 })
 
+const getCurrentUser = asyncHandler(async (req, res) => {
+    return res
+    .status(200)
+    .json(200, req.user, "current user fetched successfully")
+})
+
+
+const updateAccountDetails = asyncHandler(async (req, res) => {
+
+    const {fullName, email} = req.body
+
+    if(!fullName || !email){
+        throw new ApiError(200, "All fields are required")
+    }
+
+    const user = User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                fullName,
+                email: email
+            }
+        },
+        {new: true}
+    ).select("-password");
+
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully"))
+})
 
 export {
     registerUser,
     loginUser,
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser, 
+    updateAccountDetails
+
 }   
